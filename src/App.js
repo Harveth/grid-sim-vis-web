db27b3cfc567f3223d662cd2a5e15d16f1d22579
Belal Elsabbagh <belsabbagh@gmail.com>
2024-03-02 23:54:13 +0200

    Refactor code and update API call in GraphComponent

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import Graph from 'react-vis-network-graph';
+import React, { useState, useEffect } from "react";
+import Graph from "react-vis-network-graph";
 
-const init_color = { color: 'black' }
-const init_node_color = { color: '#36bbd6' }
+const init_color = { color: "black" };
+const init_node_color = { color: "#36bbd6" };
+
+
+async function updateState() {
+  let data = null;
+  try {
+    const response = await fetch("http://localhost:5000/next", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      }
+    });
+    data = await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+  if (data === null) {
+    return { nodes: [], edges: [] };
+  }
+  return {
+    nodes: data.meters.map((meter) => {
+      return {
+        id: meter.id,
+        label: `Node ${meter.id}`,
+        color: init_node_color,
+      };
+    }),
+    edges: data.meters
+      .filter((meter) => meter.in_trade)
+      .map((meter) => {
+        return {
+          from: meter.id,
+          to: meter.in_trade,
+          color: "red",
+        };
+      }),
+  };
+}
 
 const GraphComponent = () => {
-  const [graph, setGraph] = useState({
-    nodes: Array.from({ length: 7 }, (_, i) => ({ id: i + 1, label: `Node ${i + 1}`, color: init_node_color })),
-    edges: [
-      { from: 1, to: 2, color: init_color },
-      { from: 1, to: 3, color: init_color },
-      { from: 1, to: 4, color: init_color },
-      { from: 1, to: 5, color: init_color },
-      { from: 1, to: 6, color: init_color },
-      { from: 1, to: 7, color: init_color },
-
-      { from: 2, to: 3, color: init_color },
-      { from: 2, to: 4, color: init_color },
-      { from: 2, to: 5, color: init_color },
-      { from: 2, to: 6, color: init_color },
-      { from: 2, to: 7, color: init_color },
-
-      { from: 3, to: 4, color: init_color },
-      { from: 3, to: 5, color: init_color },
-      { from: 3, to: 6, color: init_color },
-      { from: 3, to: 7, color: init_color },
-
-      { from: 4, to: 5, color: init_color },
-      { from: 4, to: 6, color: init_color },
-      { from: 4, to: 7, color: init_color },
-
-      { from: 5, to: 6, color: init_color },
-      { from: 5, to: 7, color: init_color },
-
-      { from: 6, to: 7, color: init_color },
-
-
-    ],
-  });
+  const [graph, setGraph] = useState({});
 
   const options = {
     edges: {
-      color: '#000000', // Initial edge color
+      color: "#000000", // Initial edge color
     },
   };
 
@@ -51,9 +57,12 @@ const GraphComponent = () => {
       if (edges && edges.length === 1) {
         const selectedEdge = edges[0];
         const updatedEdges = graph.edges.map((edge) => {
-          if (edge.from === selectedEdge.from && edge.to === selectedEdge.to) {
+          if (
+            edge.from === selectedEdge.from &&
+            edge.to === selectedEdge.to
+          ) {
             // Change the color when the selected edge is found
-            return { ...edge, color: 'red' }; // Change to your desired color
+            return { ...edge, color: "red" }; // Change to your desired color
           }
           return edge;
         });
@@ -66,51 +75,49 @@ const GraphComponent = () => {
     },
   };
 
-
-
   useEffect(() => {
-    const interval = setInterval(() => {
-      const randomEdgeIndexes = Array.from({ length: 5 }, () => Math.floor(Math.random() * graph.edges.length));
-      const updatedEdges = graph.edges.map((edge, index) => {
-        if (randomEdgeIndexes.includes(index)) {
-          // get start and end node of the edge
-          const startNode = edge.from;
-          const endNode = edge.to;
-
-          //change the color of the nodes ????
-
-
-          return { ...edge, color: 'red' }; // Change to your desired color
-        }
-        return edge;
-      });
-
-      const updatedNodes = graph.nodes.map((node) => ({
-        ...node,
-        label: String(Math.floor(Math.random() * 100)),
-      }));
-
-
-
-
-      setGraph((prevGraph) => ({
-        ...prevGraph,
-        edges: updatedEdges,
-        nodes: updatedNodes,
-      }));
-    }, 1000);
-
+    const fetchDataAndUpdateGraph = async () => {
+      try {
+        const updatedGraph = await updateState();
+        setGraph(updatedGraph);
+      } catch (error) {
+        console.error("Error fetching and updating graph:", error);
+      }
+    };
+  
+    const interval = setInterval(fetchDataAndUpdateGraph, 1000);
+  
+    // Clean up function
     return () => {
       clearInterval(interval);
     };
   }, []);
-
   return (
     <div>
-      <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <h1 style={{ fontFamily: 'Arial', fontSize: '38px', textTransform: 'uppercase' }}>P2P System Visualizer</h1>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1
+          style={{
+            fontFamily: "Arial",
+            fontSize: "38px",
+            textTransform: "uppercase",
+          }}
+        >
+          P2P System Visualizer
+        </h1>
       </div>
-      <Graph graph={graph} options={options} events={events} style={{ height: '90vh' }} />
+      <Graph
+        graph={graph}
+        options={options}
+        events={events}
+        style={{ height: "90vh" }}
+      />
     </div>
   );
 };
