019232b463d3f3b8ff94c178d2a9e062d22813c3
Belal Elsabbagh <belsabbagh@gmail.com>
2024-03-03 12:29:20 +0200

    added QoL stuff

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const init_node_color = { color: "#36bbd6" };
 const GraphComponent = () => {
   const [meters, setMeters] = useState([]);
   const [time, setTime] = useState("");
+  const [fetchInterval, setFetchInterval] = useState(5000); // Default fetch interval in milliseconds
+  const [remaining, setRemaining] = useState(0); // State for remaining attribute
   const options = {
     edges: {
       color: "#000000", // Initial edge color
@@ -20,18 +22,23 @@ const GraphComponent = () => {
         if (!response.ok) return;
         const data = await response.json();
         if (data === null) return;
-        setTime(data.time)
+        setTime(data.time);
         setMeters(data.meters);
+        setRemaining(data.remaining); // Update remaining state
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
-    const interval = setInterval(fetchData, 500);
+    const interval = setInterval(fetchData, fetchInterval);
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [fetchInterval]); // Include fetchInterval in dependency array to trigger effect when it changes
+
+  const handleIntervalChange = (event) => {
+    setFetchInterval(parseInt(event.target.value));
+  };
 
   return (
     <div>
@@ -54,6 +61,21 @@ const GraphComponent = () => {
           P2P System Visualizer
         </h1>
         <h2>{time}</h2>
+        <div style={{padding: "10px"}}>
+          <label htmlFor="fetchInterval">Fetch Interval (milliseconds):</label>
+          <input
+            type="range"
+            id="fetchInterval"
+            name="fetchInterval"
+            min="100"
+            max="3000"
+            step="100"
+            value={fetchInterval}
+            onChange={handleIntervalChange}
+          />
+          <span>{fetchInterval} milliseconds</span>
+        </div>
+        <div style={{ marginBottom: "10px" }}>Remaining frames in buffer: {remaining}</div> {/* Display remaining attribute */}
         <div style={{ display: "grid", gridTemplateColumns: "repeat(8, 1fr)", gap: "10px", justifyContent: "center" }}>
           {meters.map((meter, index) => (
             <div key={index} style={{ minWidth: "200px", borderWidth: "1px", borderStyle: "solid", borderColor: ["red", "green"][(meter.surplus >= 0) * 1], padding: "8px", borderRadius: "20px"}}>
